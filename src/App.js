9923ccfd8e45f0a4a79cd5cf9fe5ac930d728eac
Alima <alimausmani21@navgurukul>
2025-03-08 09:58:06 +0530

    Initialize project structure with new folder and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Router ,Routes} from 'express';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import MovieDetails from './components/MovieDetails';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movie/:id" element={<MovieDetails/>}/>
+      </Routes>
+    </Router>
+  )
 }
-
-export default App;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div>
+      <h1>Movie Card</h1>
+    </div>
+  )
+}
+
+export default MovieCard
